6e86880f4801f58e8fb3175a1c6fd766b0981a81
Stephanie <sliu@rjmetrics.com>
2014-11-25 11:20:47 -0500

    add GTM

--- themes/ramenhog/header.php
+++ themes/ramenhog/header.php
@@ -55,6 +55,15 @@
 </head>
 
 <body class="loading" <?php body_class(); ?>>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5DB8WL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5DB8WL');</script>
+<!-- End Google Tag Manager -->
 
 <div class="pre-load"></div>
 <div id="page" class="hfeed site <?php echo $post->post_name; ?>">
